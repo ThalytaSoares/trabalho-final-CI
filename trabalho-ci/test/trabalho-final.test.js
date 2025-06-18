9f493cb237fb97c876be499e05c0fe9fea3c1fbb
Thalyta <thalytagsoares@gmail.com>
2025-06-18 13:32:43 -0300

    corrigindo estrutura de importação

--- trabalho-ci/test/trabalho-final.test.js
+++ trabalho-ci/test/trabalho-final.test.js
@@ -5,7 +5,7 @@ import {
   calcularConsumoDeRacao,
   decidirTipoDeAtividadePorPorte,
   buscarDadoAsync
-} from '..trabalho-ci/test/trabalho.js';  // ajuste o caminho se precisar
+} from './trabalho.js'; 
 
 test('QUANDO informar um nome para o Pet, DEVE ser impresso na tag com letras maiúsculas', () => {
   expect(geradorDeTagsDeIdentificacao('Pantera')).toBe('PANTERA');
